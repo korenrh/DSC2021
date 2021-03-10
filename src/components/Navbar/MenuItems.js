937719e460f7bd905832415c247c36bd1360f2fc
Reilly Koren <reillykoren@Reillys-MBP.attlocal.net>
2021-03-09 19:13:45 -0600

    Navbar

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Gallery',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Create',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact Us',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign-In',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+]
