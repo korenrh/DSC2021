21e5a874ed9d0df868a9dd9c0d9f0b5e182b4234
condrenjustin <51341538+condrenjustin@users.noreply.github.com>
2021-03-24 21:18:23 -0500

    first attempt at api, 401 error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import axios from 'axios'
 import React, { Component } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import HomePage from './index.jsx';
@@ -10,11 +11,37 @@ import { BrowserRouter as Router, Route, Switch, Link, Redirect } from "react-ro
 
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      username: ''
+    }
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  /* handleClick () {
+    axios.get('https://api.github.com/users/maecapozzi')
+      .then(response => this.setState({username: response.data.name}))
+    }
+ */
+  handleClick () {
+    axios.defaults.headers.common['Authorization'] = 'quickstart-QUdJIGlzIGNvbWluZy4uLi4K';
+    axios.post('https://api.deepai.org/api/deepdream', {
+      image: 'https://cdn.logo.com/hotlink-ok/logo-social.png'
+    })
+      .then(response => console.log("Success"))
+    }
+
   render() {
     return (
     <Router> 
       <div className="App">
-          <Navbar></Navbar>   
+          <Navbar></Navbar> 
+          <button className='button' onClick={this.handleClick}>
+            Click Me
+          </button>
+          <p>{this.state.username}</p>
       </div>
       <Switch>
         <Route exact path="/" component={HomePage} />
